053f3b91d83ad1fc4d20e27f7a606627f3657cca
Thinh Nguyen <ngthinh302@gmail.com>
2021-05-16 21:50:31 +0700

    Update app.py

--- TimMachVanh-main/app.py
+++ TimMachVanh-main/app.py
@@ -22,7 +22,7 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict_proba(final_features)
 
-    return render_template("index.html", prediction_text=f'Your chance of getting CHD in the next 10 years is {prediction[0][0] * 100:.2f}%')
+    return render_template("index.html", prediction_text=f'Khả năng mắc bệnh Tim mạch vành trong 10 năm tới là {prediction[0][0] * 100:.2f}%')
 
 @app.route('/results',methods=['POST'])
 def results():
