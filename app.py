0a786c3e6462e3eeada101ab0764565b49d9a262
Thinh Nguyen <ngthinh302@gmail.com>
2021-07-16 18:43:28 +0700

    reset

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
 from sklearn import preprocessing
@@ -8,7 +7,6 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 import statsmodels.api as sm
 from sklearn import linear_model
-from pycaret.classification import predict_model
 
 app = Flask(__name__)
 model = pickle.load(open('trained_model.pkl', 'rb'))
@@ -22,13 +20,7 @@ def predict():
 
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-
-    pred = predict_model(model, pd.DataFrame(data=final_features, 
-                                             columns=pd.Index(['male', 'age', 'education', 'currentSmoker', 'cigsPerDay', 'BPMeds',
-                                                               'prevalentStroke', 'prevalentHyp', 'diabetes', 'totChol', 'sysBP',
-                                                               'diaBP', 'BMI', 'heartRate', 'glucose'],
-                                                                dtype='object')))
-    prediction = pred["Score"][0] if pred["Label"][0] == "1.0" else 1 - pred["Score"][0]
+    prediction = model.predict_proba(final_features)
 
     return render_template("index.html", prediction_text=f'Khả năng mắc bệnh Tim mạch vành trong 10 năm tới là {prediction[0][0] * 100:.2f}%')
 
