5b7d7f0ed2670af15a5dec1a025a499e1f5eae45
Thinh Nguyen <ngthinh302@gmail.com>
2021-10-12 16:10:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def predict():
 
     int_features = [int(x) for x in request.form.values()]
     normed_features = sklearn.preprocessing.normalize([int_features], axis=1)
-    prediction = model.predict_proba(normed_features)
+    prediction = model.predict_proba([np.array(normed_features)])
 
     return render_template("index.html", prediction_text=f'Khả năng mắc bệnh Tim mạch vành trong 10 năm tới là {prediction[0][0] * 100:.2f}%')
 
